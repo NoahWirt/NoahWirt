8de400314583492790addbd62ef0beb79b071204
Noah Wirt <noahwirt@gmail.com>
2015-11-05 15:30:56 -0500

    Create PresidentialCanidateAnalysis.py
    This analysis user input to perform a complex analysis of possible presidential candidates to come to a perfect conclusion.

---
+++ PresidentialCanidateAnalysis.py
@@ -0,0 +1,11 @@
+print "This is a program that analyzes user info to find the best canidate for you!"
+a = raw_input("Are you liberal or conservative?")
+b = raw_input("Do you believe in evolution?")
+c = raw_input("Do you support abortion?")
+d = raw_input("Have you accepted Jesus as your lord and savior?")
+e = raw_input("Are you a \"socialist\" AKA COMMMUNIST?")
+f = raw_input("What is your opinion on foreign intervention?")
+g = raw_input("Free college yes or no? Otherwise: are you braindead?")
+print "PROCCESSING..................................................................................."
+print "We analyzed your info and have come to a conclusion..."
+print "Donald \"Can't stump the Trump\" Trump © is best for you!"
